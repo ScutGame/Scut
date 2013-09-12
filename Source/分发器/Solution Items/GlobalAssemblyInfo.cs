90b83c54153fbebfbc7de65d8c5007d8c7f7c75c
ScutGame <wzf_88@qq.com>
2013-09-12 18:07:33 +0800

    add dispatcher tools

---
+++ Source/分发器/Solution Items/GlobalAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.0.0.3")]
+[assembly: AssemblyFileVersion("3.0.0.3")]
+[assembly: AssemblyCompany("Scut.GameEngine")]
+[assembly: AssemblyProduct("Scut.GameEngine")]
+[assembly: AssemblyCopyright("Copyright © Scut game engine 2010-2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
