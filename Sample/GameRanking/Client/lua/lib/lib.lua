0032ef1e920cf579438766899a7aaae74c9461cc
Scutgame <zjf1998@21cn.com>
2013-10-24 00:24:00 +0800

    add client of ranking

---
+++ Sample/GameRanking/Client/lua/lib/lib.lua
@@ -0,0 +1,33 @@
+------------------------------------------------------------------
+-- lib.lua
+-- Author     : Xin Zhang
+-- Version    : 1.0.0.0
+-- Date       : 2011-10-16
+-- Description:
+------------------------------------------------------------------
+
+local strModuleName = "lib";
+--CCLuaLog("Module ".. strModuleName.. " loaded.");
+strModuleName = nil;
+
+gClassPool = {};
+
+require("lib.Common");
+require("lib.ZyMessageBoxEx");
+require("lib.ZyMultiLabel");
+require("lib.ZyButton");
+require("lib.ZyImage");
+require("lib.NetHelper");
+--require("lib.ZyToast");
+--require("lib.PointHelper");
+require("lib.ZyTable");
+require("lib.UIHelper");
+--require("lib.ZyTabBar");
+require("lib.ZyFont");
+require("lib.ZyColor");
+--require("lib.ZyPageBar");
+require("lib.ZyLinkLable")
+--require("lib.Tools");
+require("lib.XmlParser");
+require("lib.ZyBit")
+
