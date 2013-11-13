cb63ae66f490f172db9750a4251e2c55670447f7
ScutGame <wzf_88@qq.com>
2013-11-13 17:51:43 +0800

    add sample release

---
+++ Sample/Doudizhu/Server5.5/ZyGames.Doudizhu.HostServer/PyScript/Action/Action2002.py
@@ -0,0 +1,32 @@
+﻿"""2002_房间离开接口"""
+import clr, sys
+from action import *
+clr.AddReference('ZyGames.Framework.Game')
+clr.AddReference('ZyGames.Doudizhu.Lang')
+clr.AddReference('ZyGames.Doudizhu.Model')
+clr.AddReference('ZyGames.Doudizhu.Bll')
+from ZyGames.Framework.Game.Service import *
+from ZyGames.Doudizhu.Lang import *
+from ZyGames.Doudizhu.Model import *
+from ZyGames.Doudizhu.Bll.Logic import *
+
+class UrlParam(HttpParam):
+    def __init__(self):
+        HttpParam.__init__(self)
+
+class ActionResult(DataResult):
+    def __init__(self):
+        DataResult.__init__(self)
+
+def getUrlElement(httpGet, parent):
+    urlParam = UrlParam()
+    return urlParam
+
+def takeAction(urlParam, parent):
+    actionResult = ActionResult()
+    user = parent.Current.User;
+    GameRoom.Current.Exit(user)
+    return actionResult
+
+def buildPacket(writer, urlParam, actionResult):
+    return True
