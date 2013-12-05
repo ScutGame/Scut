5b0bee64f94b4682d3970d027d04c2b123b7744a
ScutGame <wzf_88@qq.com>
2013-12-04 19:27:09 -0800

    modify SvnDeployment

--- Document/Database/部署工具表结构.sql
+++ Document/Database/部署工具表结构.sql
@@ -1,5 +1,43 @@
 use SvnDeployment
 go
+CREATE TABLE SimplePlanInfo(
+	Name varchar(100) NOT NULL,
+	Value varchar(50) NULL,
+	UpdateTime datetime NULL,
+	primary key(Name)
+)
+go
+CREATE TABLE [dbo].[SimplePlanInfoLog](
+	[LogId] [int] IDENTITY(1,1) NOT NULL,
+	[ServerIp] [varchar](50) NULL,
+	[LogInfo] [varchar](500) NULL,
+	[AddTime] [datetime] NULL  DEFAULT (getdate()),
+ CONSTRAINT [PK_SimplePlanInfoLog] PRIMARY KEY CLUSTERED 
+(
+	[LogId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [dbo].[SimplePlanManager](
+	[AutoID] [int] IDENTITY(1,1) NOT NULL,
+	[PlanName] [varchar](50)  NULL,
+	[PlanPath] [varchar](300) NULL,
+	[PlanStatus] [tinyint] NULL,
+	[PlanCommand] [tinyint] NULL,
+	[CreateTime] [datetime] NULL,
+	[UpdateTime] [datetime] NULL,
+	[Remark] [varchar](50) NULL,
+	[AutoStart] [tinyint] NULL DEFAULT ((0)),
+	[ServerIP] [varchar](50) NULL,
+ CONSTRAINT [PK_SimplePlanManager] PRIMARY KEY CLUSTERED 
+(
+	[AutoID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+go
+
 --drop table DepProject
 create table DepProject
 (
@@ -13,6 +51,7 @@ create table DepProject
 	primary key(Id)
 )
 go
+
 
 --drop table DepProjectItem
 create table DepProjectItem
