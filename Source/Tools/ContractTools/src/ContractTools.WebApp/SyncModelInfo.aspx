c16708edbc723597a0512d584b945f2d7d6beaff
ScutGame <wzf_88@qq.com>
2013-12-19 02:35:53 -0800

    update sync schema

--- Source/Tools/ContractTools/src/ContractTools.WebApp/SyncModelInfo.aspx
+++ Source/Tools/ContractTools/src/ContractTools.WebApp/SyncModelInfo.aspx
@@ -9,8 +9,29 @@
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-    
+    <div style="width:1400px; float:left;padding-left:0px; margin-left:20px;">
+            <table style="width: 100%; margin-left: 0px;">
+                <tr>
+                    <td style="width:140px"><label>实体Schema配置文件:</label></td>
+                    <td>
+                        <asp:FileUpload ID="FileUpload1" runat="server"/>
+                        <span style="padding-left:5px"></span>
+                        <asp:Button ID="BtnUp" runat="server" onclick="BtnUp_Click" Text="上传Schema" />
+                        <span style="padding-left:20px"></span>
+                        <asp:Button ID="btnDown" runat="server" Text="下载Schema" onclick="btnDown_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <hr />
+                    </td>
+                </tr>
+                <tr>
+                <td colspan="2">
+                    <asp:TextBox ID="txtContent" runat="server" Height="577px" TextMode="MultiLine" Width="100%"></asp:TextBox>
+                </td>
+                </tr>
+            </table>   
     </div>
     </form>
 </body>
--- Source/Tools/ContractTools/src/ContractTools.WebApp/SyncModelInfo.aspx.designer.cs
+++ Source/Tools/ContractTools/src/ContractTools.WebApp/SyncModelInfo.aspx.designer.cs
@@ -20,5 +20,41 @@ namespace ContractTools.WebApp {
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// FileUpload1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
+        
+        /// <summary>
+        /// BtnUp 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnUp;
+        
+        /// <summary>
+        /// btnDown 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDown;
+        
+        /// <summary>
+        /// txtContent 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtContent;
     }
 }
