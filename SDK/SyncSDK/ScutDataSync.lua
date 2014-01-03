151a6bbb69e35d8ac4d2b7417422d992252480b0
ScutGame <wzf_88@qq.com>
2014-01-03 00:38:08 -0800

    Sync model for lua sdk

---
+++ SDK/SyncSDK/ScutDataSync.lua
@@ -0,0 +1,158 @@
+﻿------------------------------------------------------------------
+-- ScutDataSync.lua
+-- Author     :
+-- Version    : 1.0
+-- Date       :
+-- Description: The data sync manager
+------------------------------------------------------------------
+require("datapool.PushReceiverLayer")
+require("SyncSDK.ScutSchemaInfo")
+require("SyncSDK.ScutSchema")
+require("SyncSDK.ScutEntity")
+
+module("ScutDataSync", package.seeall)
+g__syncCallbackFunc = nil
+local g__syncActionId = 200
+
+function registerSceneCallback(func)
+    g__syncCallbackFunc = func
+end
+
+function PushErrorCallback(pScutScene, lpExternalData)
+	PushReceiverLayer.PushErrorCallback(pScutScene, lpExternalData)
+end
+
+function NotifyScene(pScutScene, lpExternalData, schemaName)
+    if g__syncCallbackFunc then
+        g__syncCallbackFunc(pScutScene, lpExternalData, schemaName)
+    end
+end
+
+function PushReceiverCallback(pScutScene, lpExternalData)
+	local actionId = ScutDataLogic.CNetReader:getInstance():getActionID()
+    if actionId == g__syncActionId then
+        local result = ScutDataLogic.CNetReader:getInstance():getResult()
+	    if result then
+	        pScutScene= CCDirector:sharedDirector():getRunningScene()
+	        PushParseEntity(pScutScene, lpExternalData)
+	    end
+    else
+        PushReceiverLayer.PushReceiverCallback(pScutScene, lpExternalData)
+    end
+end
+
+function PushParseEntity(pScutScene, lpExternalData)
+    local entityNum = ZyReader:getInt()
+    for k=1, entityNum do
+        ZyReader:recordBegin()
+        
+        local schemaName = ZyReader:readString()
+        local root = ScutEntityFactory(schemaName)
+        
+        if root then
+            local entity = root:create()
+            entity:addBegin()
+            PushReadEntity(entity)
+            entity:addEnd()
+            root:addEntity(entity)
+            
+            NotifyScene(pScutScene, lpExternalData, schemaName)
+        end
+        ZyReader:recordEnd()
+        
+    end
+end
+
+function PushReadEntity(entity, schema)
+    local schema = entity:getSchema()
+    schema:foreach(function(field)
+        if field:hasChild() then
+            local schemaName = field:getName()
+            local childSchema = field:getChild()
+            local rowNumber = ZyReader:getInt()
+            
+            if field:isDict() then
+                local keyField = childSchema:getField("_key")
+                local valueField = childSchema:getField("_value")
+                local child = nil
+                if valueField:hasChild() then
+                    child = ScutEntity:new(valueField:getChild(), {})
+                else
+                    child = ScutEntity:new(childSchema, {})
+                end
+                
+                child:addBegin()
+                for k =1, rowNumber do
+                    local pair = child:create()
+                    ZyReader:recordBegin()
+                    pair:addBegin()
+                    PushReadEntity(pair) 
+                    pair:addEnd()
+                    ZyReader:recordEnd()
+                    child:addDict(pair)
+                end
+                child:addEnd()
+                entity:addEntity(child)
+            
+            elseif field:isList() then
+                local listField = childSchema:getField("_list")
+                local child = nil
+                if listField:hasChild() then
+                    child = ScutEntity:new(listField:getChild(), {})
+                else
+                    child = ScutEntity:new(childSchema, {})
+                end
+
+                child:addBegin()
+                for k =1, rowNumber do
+                    ZyReader:recordBegin()
+                    PushReadEntity(child)
+                    ZyReader:recordEnd()
+                end
+                child:addEnd()
+                entity:addEntity(child)
+            else
+                --single child
+                local child = ScutEntity:new(childSchema, {})
+                child:addBegin()
+                ZyReader:recordBegin()
+                PushReadEntity(child)
+                ZyReader:recordEnd()
+                child:addEnd()
+                entity:addEntity(child)
+            end
+        else
+            local fieldval = PushReaderField(field:getType())
+            entity:add(fieldval)
+        end
+    end)
+        
+end
+
+function PushReaderField(fieldType)
+    local val = nil
+    if fieldType == "int" then --4 byte size
+        val = ZyReader:getInt()
+    elseif fieldType == "short" then --2 byte size
+        val = ZyReader:getWORD()
+    elseif fieldType == "string" then
+        val = ZyReader:readString()
+    elseif fieldType == "byte" then --1 byte size
+        val = ZyReader:getByte()
+    elseif fieldType == "long" then --8 byte size
+        val = ZyReader:readInt64()
+    elseif fieldType == "float" then --4 byte size
+        val = ZyReader:getFloat()
+    elseif fieldType == "double" then --8 byte size
+        val = ZyReader:getDouble()
+    elseif fieldType == "bool" then
+        val = ZyReader:getByte() == 1
+    elseif fieldType == "decimal" then
+        val = tonumber(ZyReader:readString())
+    elseif fieldType == "datetime" then
+        val = ZyReader:readString()
+    else
+        --not suport type
+    end
+    return val
+end
---
+++ SDK/SyncSDK/ScutSchemaInfo.lua
@@ -0,0 +1,124 @@
+﻿------------------------------------------------------------------
+-- ScutSchemaInfo.lua
+-- Author     :
+-- Version    : 1.0
+-- Date       :
+-- Description: The schema of entity info.
+------------------------------------------------------------------
+module("ScutSchemaInfo", package.seeall)
+--The following code is automatically generated
+g__schema = {}
+g__schema["GameUser"] = {
+    ["UserID"] = {1, "int", true},
+    ["LoginMap"] = {2, "int"},
+    ["HeadId"] = {3, "string"},
+    ["NickName"] = {4, "string"},
+    ["Sex"] = {5, "bool"},
+    ["Profession"] = {6, "int"},
+    ["UserLv"] = {7, "short"},
+    ["VipLv"] = {8, "int"},
+    ["GiftGold"] = {9, "int"},
+    ["PayGold"] = {10, "int"},
+    ["ExtGold"] = {11, "int"},
+    ["UseGold"] = {12, "int"},
+    ["BUseGold"] = {13, "int"},
+    ["GameCoin"] = {14, "int"},
+    ["BGameCoin"] = {15, "int"},
+    ["SGameCoin"] = {16, "int"},
+    ["Pid"] = {17, "string"},
+    ["RetailID"] = {18, "string"},
+    ["DeviceID"] = {19, "string"},
+    ["UserStatus"] = {20, "int"},
+    ["MsgState"] = {21, "bool"},
+    ["MobileType"] = {22, "int"},
+    ["ScreenX"] = {23, "short"},
+    ["ScreenY"] = {24, "short"},
+    ["ClientAppVersion"] = {25, "string"},
+    ["LoginTime"] = {26, "datetime"},
+    ["CreateDate"] = {27, "datetime"},
+    ["IBagNum"] = {28, "int"},
+    ["MBagNum"] = {29, "int"},
+    ["TBagNum"] = {30, "int"},
+    ["SBagNum"] = {31, "int"},
+    ["PetNum"] = {32, "int"},
+    ["TeachLv"] = {33, "int"},
+    ["EthicsNum"] = {34, "int"},
+    ["BetrayDate"] = {35, "datetime"},
+    ["BrotherID"] = {36, "string"},
+    ["MarryID"] = {37, "string"},
+    ["FactionID"] = {38, "string"},
+    ["StatusTime"] = {39, "datetime"},
+    ["Honor"] = {40, "int"},
+    ["TechUserID"] = {41, "int"},
+    ["KillCount"] = {42, "int"},
+    ["Murderous"] = {43, "int"},
+    ["ActionStatus"] = {44, "int"},
+    ["ProContribute"] = {45, "int"},
+    ["HangUpList"] = {
+        46,
+        ["_hasChild"] = true,
+        ["_list"] = {
+            1,
+            ["_hasChild"] = true,
+            ["HangUpType"] = {1, "int"},
+            ["HangUpStatus"] = {2, "int"}
+        }
+    },
+    ["RegionList"] = {
+        47,
+        ["_hasChild"] = true,
+        ["_list"] = {1, "int"}
+    },
+    ["UserFC"] = {48, "int"},
+    ["FactionMap"] = {49, "int"},
+    ["X"] = {50, "int"},
+    ["Y"] = {51, "int"},
+    ["PContributeNum"] = {52, "int"}
+}
+
+
+g__schema["UserItem"] = {
+    ["UserID"] = {1, "int", true},
+    ["ItemInfoPackage"] = {
+        2,
+        ["_hasChild"] = true,
+        ["_list"] = {
+            1,
+            ["_hasChild"] = true,
+            ["UserItemID"] = {1, "string"},
+            ["ItemID"] = {2, "int"},
+            ["Num"] = {3, "int"},
+            ["Level"] = {4, "int"},
+            ["ItemInType"] = {5, "int"},
+            ["IsCarve"] = {6, "bool"},
+            ["IsBind"] = {7, "bool"},
+            ["UserEqui"] = {
+                8,
+                ["_hasChild"] = true,
+                ["Endurance"] = {1, "int"},
+                ["Resupply"] = {2, "int"},
+                ["ForgeLv"] = {3, "int"},
+                ["GemID"] = {4, "int"},
+                ["GemNum"] = {5, "int"},
+                ["Addition"] = {
+                    6,
+                    ["_hasChild"] = true,
+                    ["_list"] = {
+                        1,
+                        ["_hasChild"] = true,
+                        ["AttrType"] = {1, "int"},
+                        ["AttrNum"] = {2, "int"},
+                        ["PreNum"] = {3, "decimal"},
+                        ["BaseAttrNum"] = {4, "int"}
+                    }
+                },
+                ["PetID"] = {7, "string"},
+                ["Special"] = {8, "int"},
+                ["PourID"] = {9, "int"},
+                ["PourLv"] = {10, "int"}
+            }
+        }
+    }
+}
+
+
