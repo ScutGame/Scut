ea4ab07b9c7a9d8ce8425c33a12602b87876617a
ScutGame <wzf_88@qq.com>
2014-01-06 18:13:21 -0800

    remove listempty.bat

--- listempty.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-for /f "delims=" %%i in ('dir /ad-h /b /s') do (
-set n=0
-for /f "delims=" %%j in ('dir "%%i" /a /b') do set /a n+=1
-if !n!==0 copy readme.md "%%i"
-)
-
-pause
