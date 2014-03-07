d477f0a68bb3322c1294daa139a6394fb509dc21
ScutGame <wzf_88@qq.com>
2014-03-06 20:06:10 -0800

    Release lib 6.1.6.2

---
+++ Release/6.1.6.2/Console/Script/PyScript/Lib/ReferenceLib.py
@@ -0,0 +1,6 @@
+import clr, sys
+clr.AddReference('ZyGames.Framework.Common')
+clr.AddReference('ZyGames.Framework')
+clr.AddReference('ZyGames.Framework.Game')
+clr.AddReference('zt3mntet')
+clr.AddReference('qlmxrcpx')
