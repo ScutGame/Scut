30319d04bdc1788311e6b17ab2e7a4cff81c5b2f
ScutGame <wzf_88@qq.com>
2014-03-14 16:26:08 +0800

    release lib 6.1.6.5

--- Sample/GameRanking/Server/src/GameRanking.Web/Script/PyScript/Lib/ReferenceLib.py
+++
@@ -1,6 +0,0 @@
-import clr, sys
-clr.AddReference('ZyGames.Framework.Common')
-clr.AddReference('ZyGames.Framework')
-clr.AddReference('ZyGames.Framework.Game')
-clr.AddReference('dgouutw1')
-clr.AddReference('xbyln0pq')
