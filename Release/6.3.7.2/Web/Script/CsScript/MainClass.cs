bd6fd06348f7f8dc3169516172e9d5bc3631f364
ScutGame <wzf_88@qq.com>
2014-04-25 11:25:05 +0800

    update framework lib  6.3.7.2

---
+++ Release/6.3.7.2/Web/Script/CsScript/MainClass.cs
@@ -0,0 +1,18 @@
+using System;
+using ZyGames.Framework.Common.Log;
+using ZyGames.Framework.Script;
+
+namespace Game.Script
+{
+    public class MainClass : IMainScript
+    {
+        public void Start(string[] args)
+        {
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
