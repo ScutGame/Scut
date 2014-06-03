89191194457dc8e44dcd598f25aaf3d99efa8d0d
ScutGame <wzf_88@qq.com>
2014-06-03 13:21:04 +0800

    update framework lib  6.5.8.0

---
+++ Release/6.5.8.0/Console/Script/LuaScript/Action/Action1001.lua
@@ -0,0 +1,19 @@
+﻿Action1001 ={}
+
+function Action1001:getUrlElement(httpGet, parent)
+	local urlParam = {}
+	urlParam.Result = true
+    return urlParam
+end
+
+function Action1001:takeAction(urlParam, parent)
+	local actionResult = {}
+	actionResult.Result = true
+    return actionResult
+end
+
+
+function Action1001:buildPacket(writer, urlParam, actionResult)
+    return true
+end
+
---
+++ Release/6.5.8.0/Web/Service.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Service.aspx.cs" Inherits="GameWebServer.Service" %>
