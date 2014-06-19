92be428bc6eb5013bac5d8b8db689e24f66814db
ScutGame <wzf_88@qq.com>
2014-06-19 18:48:53 +0800

    update sample for 6.5.8.2

--- Sample/Doudizhu/Server/release/Script/PyScript/Lib/ReferenceLib.py
+++ Sample/Doudizhu/Server/release/Script/PyScript/Lib/ReferenceLib.py
@@ -2,5 +2,5 @@ import clr, sys
 clr.AddReference('ZyGames.Framework.Common')
 clr.AddReference('ZyGames.Framework')
 clr.AddReference('ZyGames.Framework.Game')
-clr.AddReference('swhzpayk')
-clr.AddReference('d4d44255')
+clr.AddReference('gige1iqg')
+clr.AddReference('4sfyhcfq')
