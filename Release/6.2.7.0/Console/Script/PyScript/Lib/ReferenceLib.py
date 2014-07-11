c208b86bdc65a2a027af3a53bc4d0cf4155ccd73
ScutGame <wzf_88@qq.com>
2014-07-11 16:53:51 +0800

    remove old lib

--- Release/6.2.7.0/Console/Script/PyScript/Lib/ReferenceLib.py
+++
@@ -1,6 +0,0 @@
-import clr, sys
-clr.AddReference('ZyGames.Framework.Common')
-clr.AddReference('ZyGames.Framework')
-clr.AddReference('ZyGames.Framework.Game')
-clr.AddReference('zt3mntet')
-clr.AddReference('qlmxrcpx')
--- Release/6.3.7.3/Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameWebServer.Global" Language="C#" %>
