70a641723d5c3bfea00edb4b6767861fc909dcef
ScutGame <wzf_88@qq.com>
2015-03-12 19:13:20 +0800

    update json

--- Release/PythonLib/Lib/action.py
+++ Release/PythonLib/Lib/action.py
@@ -10,6 +10,21 @@ class DataResult(object):
     def __init__(self):
         self.Result = True
 
+class JsonDataResult():
+    """Json处理结果"""
+
+    def __init__(self, urlParam):
+        self.MsgId = urlParam.MsgId
+        self.ActionId = urlParam.ActionId
+        self.ErrorCode = 0
+        self.ErrorInfo = ''
+        self.Data = None
+
+    def setBody(self, value):
+        self.Data = value
+
+
 def getGbLen(str):
     """获得中英文长度"""
     return len(str.encode('gb2312'))
+
