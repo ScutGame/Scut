3a052a0e67928f8d5cfe1229e07d9cd95660782e
ScutGame <wzf_88@qq.com>
2015-03-17 13:53:21 +0800

    update source

---
+++ Source/Framework/ZyGames.Framework/Model/ISqlEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZyGames.Framework.Model
+{
+    /// <summary>
+    /// Supported sql entity object.
+    /// </summary>
+    public interface ISqlEntity
+    {
+
+        /// <summary>
+        /// Message queue group id.
+        /// </summary>
+        /// <returns></returns>
+        int GetMessageQueueId();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        bool IsDelete { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void ResetState();
+    }
+}
