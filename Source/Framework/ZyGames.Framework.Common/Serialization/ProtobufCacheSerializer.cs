ba27ecac840f3132c0e831395c6e67e22c685980
ScutGame <zjf_1998@qq.com>
2015-07-23 14:17:56 +0800

    update bug

--- Source/Framework/ZyGames.Framework.Common/Serialization/ProtobufCacheSerializer.cs
+++ Source/Framework/ZyGames.Framework.Common/Serialization/ProtobufCacheSerializer.cs
@@ -48,7 +48,7 @@ namespace ZyGames.Framework.Common.Serialization
         /// <returns></returns>
         public object Deserialize(byte[] data, Type type)
         {
-            return ProtoBufUtils.Deserialize(data, type, false);
+            return ProtoBufUtils.Deserialize(data, type);
         }
     }
 }
